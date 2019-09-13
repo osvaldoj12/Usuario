468cbb56c8a8ee4e88bbb4b0ad0a99b539b30eae
Osvaldo <osvaldojuniorlima@hotmail.com>
2019-09-13 14:28:57 -0300

    Importando o Usuario e Instanciando

---
+++ user_pt2.py
@@ -0,0 +1,12 @@
+from Usuarios import User
+
+user_1 = User("Rafael", "Batista", "Limoeiro", "Espanhol", 23)
+user_2 = User("China", "Lee", "Acre", "Indiano", 45)
+user_3 = User("Mago", "Arabia", "Ferreiros", "Portugues", 126)
+
+user_1.describe_user()
+user_1.greet_user()
+user_2.describe_user()
+user_2.greet_user()
+user_3.describe_user()
+user_3.greet_user()
